465663371545682601c8df88bf813933cc89e82a
Sasha Demyanik <ttsvan@msn.com>
2018-10-18 22:09:25 -0700

    Updates

---
+++ precache-manifest.66b5e9b6a2b228c1878f81dd963bde36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cacdfd08ce3ded2ac77",
+    "url": "/static/js/main.1cacdfd0.chunk.js"
+  },
+  {
+    "revision": "8d9700315185b37d8201",
+    "url": "/static/js/1.8d970031.chunk.js"
+  },
+  {
+    "revision": "1cacdfd08ce3ded2ac77",
+    "url": "/static/css/main.8c6666a5.chunk.css"
+  },
+  {
+    "revision": "dcc79da82bef1ff487a5c5c3060185bd",
+    "url": "/index.html"
+  }
+];
